b36b433af61c798016753d77dc11a081b44ef5cf
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 01:40:07 +0800

    Added an input for list from user

--- display_numbers_divisible_by_5_from_a_list.py
+++ display_numbers_divisible_by_5_from_a_list.py
@@ -2,5 +2,8 @@
 # Iterate the given list of numbers and print only those numbers which are divisible by 5
 
 # Request input for list from user
+
+number_list = [int(input("First Number: ")), int(input("Second Number: ")), int(input("Third Number: ")), int(input("Fourth Number: ")), int(input("Fifth Number: "))]
+
 # Print the list
 # Print numbers that are divisible by 5
