7dd04e0e2286f5184edb5a2f5ac5878819f69699
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 01:36:18 +0800

    Program Instructions and Pseudocode

---
+++ display_numbers_divisible_by_5_from_a_list.py
@@ -0,0 +1,6 @@
+# Assignment 5 - Exercise 6
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+# Request input for list from user
+# Print the list
+# Print numbers that are divisible by 5
