c350de9ae92bd8624c741df514f11238b844dbfd
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 01:45:40 +0800

    Added the printing of output of divisible by 5 numbers

--- display_numbers_divisible_by_5_from_a_list.py
+++ display_numbers_divisible_by_5_from_a_list.py
@@ -6,4 +6,13 @@
 number_list = [int(input("First Number: ")), int(input("Second Number: ")), int(input("Third Number: ")), int(input("Fourth Number: ")), int(input("Fifth Number: "))]
 
 # Print the list
+
+print("\nGiven List:", number_list)
+
 # Print numbers that are divisible by 5
+
+print("\nNumbers divisible by 5: ")
+
+for number in number_list:
+    if number % 5 == 0:
+        print(number)
